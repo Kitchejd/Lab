3af253c53da9f997f7ca1daa5f82c790173e6156
Kitchejd <142835429+Kitchejd@users.noreply.github.com>
2023-10-01 10:50:12 -0400

    Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,10 +1,11 @@
-
-days=int(input('Enter days: ')) * 24 * 3600
-months=int(input('Enter Month: ')) *12 *24 *3600
-years=int(input('Enter Year: ')) * 365 * 24 * 3600
+import time
+days = int(input('Enter days: ')) * 24 * 3600
+months = int(input('Enter Month: ')) *12 *24 *3600
+years = int(input('Enter Year: ')) * 365 * 24 * 3600
 
 time = days + months + years
 
+time.strftime(%S)
 
 print ('This is the time in seconds' , time)
                 
